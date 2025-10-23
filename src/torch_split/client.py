577d91916f4dec1f231dfc9cffc0b982a038923b
Jeffrey Qian <jeffreyqdd@gmail.com>
2025-10-22 21:45:37 -0400

    remove circular import

--- src/torch_split/client.py
+++ src/torch_split/client.py
@@ -4,8 +4,6 @@ from typing import Any
 import torch
 from torch.nn import Module
 
-from torch_split.profiling.profiler import InstrumentedModel
-
 
 class TorchSplitClient(ABC):
     @abstractmethod
@@ -18,9 +16,9 @@ class TorchSplitClient(ABC):
         """Return the model."""
         raise NotImplementedError("SplitInterface.get_model is not implemented")
 
-    @abstractmethod
-    def benchmark_model(self, model: InstrumentedModel):
-        pass
+    # @abstractmethod
+    # def benchmark_model(self, model: InstrumentedModel):
+    #     pass
 
     @abstractmethod
     def target_device(self) -> torch.device:
